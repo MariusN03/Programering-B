95e5d9e50c1d026499b3de870149ae4c587f8e33
MariusN03 <mari71k1@edu.nextkbh.dk>
2021-09-20 13:26:19 +0200

    arbejde med array og søgefelt

---
+++ 09-javascript-arrays/index.js
@@ -0,0 +1,36 @@
+let main = document.querySelector('main') 
+let input = document.querySelector('#input')
+
+const addCard = name => {
+let newCard = document.createElement('div')
+newCard.classList.add('card')
+let newH = document.createElement('h1')
+newH.innerHTML = name
+newCard.append(newH)
+main.append(newCard)
+}
+
+let names = ['Pogba', 'Ronaldo', 'Sancho', 'De Gea']
+
+let numbers = [1,23,34]
+
+
+names.push('Nagib')
+
+names.map(n => addCard(n))
+
+
+input.addEventListener('input', () =>{
+    let results = names.filter(n => n.toLowerCase().includes(input.value.toLowerCase()))
+    main.innerHTML = ''
+    results.map(n => addCard(n))
+})
+
+
+
+
+
+// her er et for loop
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i])
+// }
