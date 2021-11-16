77f0b92426f9ac45e226e8f65f2cf19220458efe
MariusN03 <mari71k1@edu.nextkbh.dk>
2021-11-16 22:04:55 +0100

    Whos that pokemon kalender funktion

--- opgave-9-Kalender/index.js
+++ opgave-9-Kalender/index.js
@@ -1,21 +1,55 @@
 let api_key = "u4QsbdJ55v6W3irNyXGgT0Hg3O6W7jSK"
+let img = document.querySelector('img')
+let bruh
 
-fetch("https://api.giphy.com/v1/gifs/trending?api_key=" + api_key + "&limit=20&rating=pg-13")
+/*fetch("" + value + ".png")
     .then(response => response.json()
     .then(json => {
         console.log(json)
-        json.data.map( image => insertImage(image.images.fixed_height.url))
-        /*for(let i = 0; i < 12; i++) {
+        json.map( image => createPokeImage(image))
+        for(let i = 0; i < 12; i++) {
             let bruh = document.createElement('div')
             bruh.classList.add('boxing')
             bruh.innerHTML = "<img src=" + json.data[i].images.fixed_height.url + "></br>"
             document.querySelector('body').append(bruh)
-        }*/
+        }
         }))
         
-    insertImage = (url) => {
-        let bruh = document.createElement('img')
-            bruh.classList.add('boxing')
-            bruh.src = url
-            document.querySelector('body').append(bruh)
+        function createPokeImage(pokeID, containerDiv){
+            let pokeImage = document.createElement('img')
+            pokeImage.src =`https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
+            containerDiv.append(pokeImage);
+          }*/
+
+
+    for(let i = 1 ; i<25 ; i++){
+        let pokebob = document.createElement('div')
+        pokebob.classList.add('boxing')
+        let l = "0" + i 
+        if(i>9){
+            l = i
+        }
+        pokebob.innerHTML = "<img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/0" + l + ".png'>"
+        
+        document.querySelector('body').append(pokebob)
+        console.log(pokebob.innerHTML)
+        
     }
+
+    let all = document.getElementsByClassName('boxing');
+    for (var i = 0; i < all.length; i++) {
+        all[i].style.filter = 'brightness(0%)';
+    }
+
+
+const datPokemon = () => {
+for (var i = 0; i < all.length; i++) {
+   if( all[i].style.filter == 'brightness(0%)'){
+  all[i].style.filter = 'brightness(100%)';}
+  else{
+      all[i].style.filter = 'brightness(0%)';
+  }
+}
+}
+    
+document.querySelector('#wdp').addEventListener('click', datPokemon)
