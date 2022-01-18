b6870d815e0d946fe834a949017ea011d8b18673
MariusN03 <mari71k1@edu.nextkbh.dk>
2022-01-18 14:18:21 +0100

    flappybird animation + point colision

--- 113-P5-FLAPPY/index.js
+++ 113-P5-FLAPPY/index.js
@@ -6,14 +6,25 @@ let updrift = 25
 let rectX, rectY, rectW, rectH
 let rectSpeed = 10
 let score = 0
+let bird1,bird2,bird3,bird4,bird5
+const animRate = 50
+
+
+function preload() {
+    bird1 = loadImage('assets/bird1.png');
+    bird2 = loadImage('assets/bird2.png');
+    bird3 = loadImage('assets/bird3.png');
+    bird4 = loadImage('assets/bird4.png');
+    bird5 = loadImage('assets/bird5.png');
+  }
 
 function setup(){
     createCanvas(windowWidth, windowHeight)
     frameRate(60)
     background('green')
     x = windowWidth/2
-    diameter = 32
-    y = diameter/2
+    diameter = 110
+    y = 200
     rectW = 20
     rectH = 100
     rectX = windowWidth
@@ -21,6 +32,8 @@ function setup(){
 }
 
 function showRect(){
+    stroke('black')
+    fill('white')
     rect(rectX, rectY, rectW, rectH)
     rect(rectX, 0, rectW, rectH)
 }
@@ -35,7 +48,18 @@ function updateRect(){
 }
 
 function show(){
-    ellipse(x, y, diameter)
+    ellipseMode(CENTER)
+    imageMode(CENTER)
+    /*fill(0,0,0,0)
+    stroke(0,0,0,0)
+    ellipse(x, y, diameter)*/  
+    
+    let a = frameCount % animRate/10
+    if(a<1)image(bird1, x, y)
+    if(a>=1&&a<2)image(bird2, x, y)
+    if(a>=2&&a<3)image(bird3, x, y)
+    if(a>=3&&a<4)image(bird4, x, y)
+    if(a>=4)image(bird5, x, y)
 }
 
 function update(){
@@ -47,6 +71,26 @@ function update(){
         y = windowHeight - diameter/2
         velocity = -velocity
     }
+    if(y < 0 + diameter/2){
+        y = 0 + diameter/2
+        score +=4
+        velocity = -velocity
+    }
+}
+
+function colision(){
+    //cirklens nederste punkt er x, y+diameter/2
+    //cirklens øverste punkt er x, y-diameter/2
+    /*let d = dist(x, y, rectX, rectY)
+    if(d<diameter/2){
+        score -=1
+        y=rectY-diameter/2
+    }*/
+    if(x > rectX && x < rectX + rectW){
+        if(y < rectH || y > windowHeight - rectH){
+            score -= 1      
+        }
+    }
 }
 
 function draw(){
@@ -56,6 +100,7 @@ function draw(){
     showRect()
     updateRect()
     select('#info').html(score)
+    colision()
 }
 
 function keyPressed(key){
