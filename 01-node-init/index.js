8fbd309c4551f1de2efe560ff957bc56e370b8bb
MariusN03 <mari71k1@edu.nextkbh.dk>
2022-04-05 14:15:42 +0200

    mqtt setup

--- 01-node-init/index.js
+++ 01-node-init/index.js
@@ -1,7 +1,19 @@
-console.log('yoooo')
-
 const express = require('express')
 const app = express()
+const mqtt = require('mqtt')
+const client  = mqtt.connect('wss://mqtt.nextservices.dk')
+
+client.on('connect', function () {
+  console.log('connected nextservices')
+  console.log('subscribing to programmering...')
+  client.subscribe('programmering')
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  // client.end()
+})
 
 app.get('/', function (req, res) {
   res.send('Du lugter ;)')
